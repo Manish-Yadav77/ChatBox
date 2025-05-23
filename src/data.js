14d8d9a3b632d6fed4551a40d425ef87d5102879
Manish-Yadav77 <manishkyadav969@gmail.com>
2025-05-23 19:19:34 +0530

    update

--- src/data.js
+++ src/data.js
@@ -1,9 +1,9 @@
-import project1 from './assets/project1.jpg';
-import project2 from './assets/project2.jpg';
-import project3 from './assets/project3.jpg';
+import project1 from './assets/Project1.jpg';
+import project2 from './assets/Project2.jpg';
+import project3 from './assets/Project3.jpg';
 import bg1 from './assets/bg1.png';
 import bg2 from './assets/bg2.png';
 import bg3 from './assets/bg3.png';
-import userProfile from './assets/userProfile.png';
+import userProfile from './assets/UserProfile.png';
 
 export {project1, project2, project3, bg1, bg2, bg3, userProfile};
